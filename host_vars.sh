f3bacc6d5bedc96bc40f45b65e53cb32901e7dd9
vuiseng9 <vuiseng9@gmail.com>
2024-10-04 15:35:24 -0700

    Add host specific variables host_vars.sh

--- host_vars.sh
+++ host_vars.sh
@@ -4,14 +4,39 @@ declare -A vsrunroot
 declare -A vscondaroot
 declare -A vshfroot #hf cache
 
-vshomeroot['MSI']=/workspace
+vshomeroot['MSI']=/home/vuiseng9
 vsdevroot['MSI']=/workspace/dev
 vsrunroot['MSI']=/workspace/run
 vscondaroot['MSI']=/home/vuiseng9/miniforge3
+# vshfroot['MSI']=
 
 HOSTID=$(uname -n)
-VS_HOME_DIR=${homeroot[$HOSTID]}
-VS_DEV_DIR=${devroot[$HOSTID]}
-VS_RUN_DIR=${runroot[$HOSTID]}
-VS_CONDA_DIR=${condaroot[$HOSTID]}
+VS_HOME_DIR=${vshomeroot[$HOSTID]}
+VS_DEV_DIR=${vsdevroot[$HOSTID]}
+VS_RUN_DIR=${vsrunroot[$HOSTID]}
+VS_CONDA_DIR=${vscondaroot[$HOSTID]}
 VS_HFCACHE_DIR=${vshfroot[$HOSTID]}
+
+if ! [ -z $VS_HFCACHE_DIR ] ; then
+    export HF_HOME=$VS_HFCACHE_DIR
+fi
+
+vshome() {
+    cd $VS_HOME_DIR
+}
+
+vsdev() {
+    cd $VS_DEV_DIR
+}
+
+vsrun() {
+    cd $VS_RUN_DIR
+}
+
+vsconda() {
+    cd $VS_CONDA_DIR
+}
+
+vshf() {
+    cd $VS_HFCACHE_DIR
+}
