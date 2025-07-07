22fc502a17c9a6fcd1982855988f191ac8f961dc
Vui Seng Chua <vuiseng9@gmail.com>
2025-07-07 12:47:15 -0700

    Adapt for shadeform instance * modify/add shadeform_instance_setup.sh host_vars.sh

--- host_vars.sh
+++ host_vars.sh
@@ -5,18 +5,18 @@ declare -A vscondaroot
 declare -A vshfroot #hf cache
 
 HOSTID=$(uname -n)
-vshomeroot[$HOSTID]=/home/ubuntu/work
-vscondaroot[$HOSTID]=/home/ubuntu/miniforge3
-vsdevroot[$HOSTID]=/home/ubuntu/work/dev
-vsrunroot[$HOSTID]=/home/ubuntu/work/run
-vshfroot[$HOSTID]=/home/ubuntu/work/huggingface
+vshomeroot[$HOSTID]=/home/$USER/work
+vscondaroot[$HOSTID]=/home/$USER/miniforge3
+vsdevroot[$HOSTID]=/home/$USER/work/dev
+vsrunroot[$HOSTID]=/home/$USER/work/run
+vshfroot[$HOSTID]=/home/$USER/work/huggingface
 
 VS_HOME_DIR=${vshomeroot[$HOSTID]}
 VS_DEV_DIR=${vsdevroot[$HOSTID]}
 VS_RUN_DIR=${vsrunroot[$HOSTID]}
 VS_CONDA_DIR=${vscondaroot[$HOSTID]}
 VS_HFCACHE_DIR=${vshfroot[$HOSTID]}
-VS_PREFIX_CONDAENV=lmd-$(datestr) # to be modified manually
+VS_PREFIX_CONDAENV=sf-$(datestr) # to be modified manually
 
 if ! [ -z $VS_HFCACHE_DIR ] ; then
     export HF_HOME=$VS_HFCACHE_DIR
